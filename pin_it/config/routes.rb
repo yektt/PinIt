02a0f2cd0bd178556c5dbd99dac3960c5b1d8acf
Yekta <yektaturan.93@gmail.com>
2020-09-24 14:38:47 +0200

    starting again

--- pin_it/config/routes.rb
+++ pin_it/config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'user/register'
-
-  get 'user/new'
-
-  get 'user/log_in'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
