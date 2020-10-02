e14e69551f86d020a6b82e4cc3ce944250506b73
Yekta <yektaturan.93@gmail.com>
2020-10-02 15:19:16 +0200

    created Pin model with title, tag and image_url attributes

--- pin_it/db/schema.rb
+++ pin_it/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200924142037) do
+ActiveRecord::Schema.define(version: 20201002131628) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "title"
+    t.string "tag"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
