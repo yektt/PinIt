4f353b6c48fa87955d0905edb45fdbcd0bcda7d8
Yekta <yektaturan.93@gmail.com>
2020-10-05 16:24:19 +0200

    first comment model test

--- pin_it/test/models/comment_test.rb
+++ pin_it/test/models/comment_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'changing the associated pin for a comment' do
+    pin = Pin.new title: 'coffee with friends'
+    pin.save!
+
+    comment1 = Comment.new body: "I'd like to come with you", pin: pin
+    comment1.save
+
+    pin2 = Pin.new title: 'tea with friends'
+    pin2.save
+
+    comment1.pin = pin2
+    comment1.save
+
+    refute_equal pin, comment1.pin
+  end
+
 end
