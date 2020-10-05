98585f2bea19fb45f9eb2e14c2ecd483a0eca367
Yekta <yektaturan.93@gmail.com>
2020-10-05 17:41:05 +0200

    add user references to comment model

--- pin_it/db/schema.rb
+++ pin_it/db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201005142010) do
+ActiveRecord::Schema.define(version: 20201005154026) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
     t.integer "pin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["pin_id"], name: "index_comments_on_pin_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "pins", force: :cascade do |t|
