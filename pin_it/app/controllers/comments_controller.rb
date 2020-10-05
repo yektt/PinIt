e78835c8d1aeb4734fcefdf99768c1ea2cab8e7b
Yekta <yektaturan.93@gmail.com>
2020-10-05 17:08:16 +0200

    process comment form

--- pin_it/app/controllers/comments_controller.rb
+++ pin_it/app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  def create
+    comment = Comment.new (comment_params)
+    pin = Pin.find(params[:pin_id])
+    comment.pin = pin
+    comment.save!
+
+    redirect_to pin_path(pin)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
