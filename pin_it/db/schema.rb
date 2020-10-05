72a198df69e1fbd94278b8a7a2f9e8791b473aaf
Yekta <yektaturan.93@gmail.com>
2020-10-05 16:21:12 +0200

    created comment model associated with Pins

--- pin_it/db/schema.rb
+++ pin_it/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201002131628) do
+ActiveRecord::Schema.define(version: 20201005142010) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "pin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pin_id"], name: "index_comments_on_pin_id"
+  end
 
   create_table "pins", force: :cascade do |t|
     t.string "title"
