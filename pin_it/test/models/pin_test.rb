620bd53bd80111a280b2c0e8d056083878d08b8a
Yekta <yektaturan.93@gmail.com>
2020-10-05 12:44:08 +0200

    add model test for changing title

--- pin_it/test/models/pin_test.rb
+++ pin_it/test/models/pin_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PinTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'changing the title' do
+    pin = Pin.new title: 'title of the pin'
+    pin.save!
+
+    updated_at = pin.updated_at
+
+    pin.title = 'new title of the pin'
+
+    assert pin.save!
+    refute_equal pin.updated_at, updated_at
+  end
+
 end
