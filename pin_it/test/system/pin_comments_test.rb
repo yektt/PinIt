9a66f52c2a1328377ff98f539f787ce7c9f65a43
Yekta <yektaturan.93@gmail.com>
2020-10-05 17:20:27 +0200

    checked if the comment system is working for comment adding

--- pin_it/test/system/pin_comments_test.rb
+++ pin_it/test/system/pin_comments_test.rb
@@ -1,9 +1,17 @@
 require "application_system_test_case"
 
 class PinCommentsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit pin_comments_url
-  #
-  #   assert_selector "h1", text: "PinComment"
-  # end
+  
+  test 'adding a comment to an pin' do
+    pin = Pin.new title: 'title'
+    pin.save
+
+    visit (pin_path(pin)) 
+    fill_in('Add a comment', with: 'comment')
+    click_on('Post', match: :first)
+
+    assert_equal pin_path(pin), page.current_path
+    assert page.has_content? ('comment')
+  end
+
 end
