4aab28bb4540a65ded440232fdbc1b9bbef8d3ce
Yekta <yektaturan.93@gmail.com>
2020-10-05 17:07:52 +0200

    created pin_comment test file

---
+++ pin_it/test/system/pin_comments_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class PinCommentsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit pin_comments_url
+  #
+  #   assert_selector "h1", text: "PinComment"
+  # end
+end
