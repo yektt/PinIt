f8a46955461ea1095afc5d1018cc82872758d3f5
Yekta <yektaturan.93@gmail.com>
2020-10-07 14:43:22 +0200

    created 'create action' for goals controller

--- pin_it/app/controllers/goals_controller.rb
+++ pin_it/app/controllers/goals_controller.rb
@@ -1,2 +1,11 @@
 class GoalsController < ApplicationController
+  def create
+    user = User.find(params[:user_id])
+    pin = Pin.find(params[:pin_id])
+
+    user.goals << pin
+
+    redirect_to pin_path(pin)
+  end
+
 end
