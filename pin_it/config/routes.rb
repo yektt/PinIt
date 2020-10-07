18482dfeb2d2cb95fcb7a393e63426316eb06de9
Yekta <yektaturan.93@gmail.com>
2020-10-07 14:26:19 +0200

    adds a goals resource

--- pin_it/config/routes.rb
+++ pin_it/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
-  resources :users
+  resources :users do
+    resources :goals
+  end
   
   resources :pins do
    resources :comments
